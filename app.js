ad3f98ece3ad1a1272073f8f1a6b25223c061391
OctavioM3 <octaviom3@nycstudents.net>
2023-10-02 09:23:04 -0400

    hjghj

---
+++ app.js
@@ -0,0 +1,20 @@
+// declare and define function, input output machine
+//we can pass in arguments or parameters
+function test(mess) {
+    console.log(mess);
+}
+//run the function
+test(true);
+
+//String ""
+const x = "String";
+//declaring a variable, defining a variable
+test(x);
+
+//Numer/Integer
+const y = 98
+test(y);
+
+//Boolean true or false
+const z = true
+test(z);
