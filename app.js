3e51b747f195418367c9ec2494af79792b9a0372
OctavioM3 <octaviom3@nycstudents.net>
2023-10-05 09:23:35 -0400

    utyytu

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-function calcBMI (WeightKg, HeightCm) {
-    if (WeightKg <= 0 || HeightCm <= 0) {
-        return "Invalid input. Weight and height must be positive numbers.";
-    }
-
-    const BMI = (WeightKg / HeightCm **2) * 703;
-    return BMI;
-}
 let WeightKg = Number(prompt("Enter weight in kilograms"));
 let HeightCm = Number(prompt("Enter height in centimeters"));
-console.log(calcBMI)
+function calcBMI (WeightKg, HeightCm) {
+    const BMI = ((WeightKg / (WeightKg * HeightCm)) * 703);
+    return BMI;
+}
+console.log(BMI)
+
+const User = {
+    firstName: "",
+    WeightKg: 80,
+    HeightCm: 240,
+};
+console.log();
