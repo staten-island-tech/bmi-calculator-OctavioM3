be9fa5f6b2d75cef497799af72d67a068f6a53f8
OctavioM3 <octaviom3@nycstudents.net>
2023-10-05 23:17:04 -0400

    dsdasdasd

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 let WeightKg = Number(prompt("Enter weight in kilograms"));
 let HeightCm = Number(prompt("Enter height in centimeters"));
-function calcBMI (WeightKg, HeightCm) {
-    return BMI = ((WeightKg / (WeightKg * HeightCm)) * 703);
+function calcBMI(WeightKg, HeightCm) {
+    return ((WeightKg / (WeightKg * HeightCm)) * 10000);
 }
-console.log(BMI)
 
-const User = {
-    firstName: "",
-    WeightKg: 80,
-    HeightCm: 240,
-};
-console.log();
+function BMIrange() {
+    if (BMI < 18.5){
+        return ("underweight");
+    } else if (BMI < 25){
+        return ("healthy");
+    } else if (BMI < 30){
+        return ("overweight");
+    } else {
+        return ("obese");
+    }
+}
+
+const BMI =
+calcBMI(WeightKg, HeightCm);
+const range = BMIrange(WeightKg, HeightCm);
+
+console.log("User BMI " + BMI);
+console.log("User BMI range " + range);
